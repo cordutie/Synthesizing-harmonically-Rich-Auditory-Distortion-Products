51098bcd7a6c0a32fe0c0397365bd5527a0630a4
Esteban <egutierreo@uc.cl>
2023-03-11 20:01:08 -0300

    First version: able to test the equation from 1 to 12 equations.

---
+++ submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Nombre del trabajo
+#SBATCH --job-name=Neural-Sweet
+# Archivo de salida
+#SBATCH --output=salida.txt
+# Cola de trabajo
+#SBATCH --partition=full
+# Reporte por correo
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=egutierreo@uc.cl
+# Solicitud de cpus
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+
+python main.py
