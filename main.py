4db62bf7df4d99de1409400f797ca0de7d205a87
Esteban <egutierreo@uc.cl>
2023-03-26 00:48:45 -0300

    First version: able to test the equation from 1 to 8 equations.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from multiprocessing import Pool
 
 if __name__ == '__main__':
     # List of Parameters
-    parameters = [i for i in range(1, 6)]
+    parameters = [i for i in range(1, 9)]
 
     # Multiprocessing
     with Pool(processes=None) as p:
